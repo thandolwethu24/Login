25a9d31ba5339397cdae7f9cef3ee75089ee69d0
thandolwethu24 <thandolwethus.axxess@gmail.com>
2021-08-14 12:30:13 +0200

    Initial commit

---
+++ app/models/Users.php
@@ -0,0 +1,116 @@
+<?php
+
+use Phalcon\Validation;
+use Phalcon\Validation\Validator\Email as EmailValidator;
+
+class Users extends \Phalcon\Mvc\Model
+{
+
+    /**
+     *
+     * @var integer
+     */
+    public $id;
+
+    /**
+     *
+     * @var string
+     */
+    public $name;
+
+    /**
+     *
+     * @var string
+     */
+    public $email;
+
+    /**
+     *
+     * @var string
+     */
+    public $password;
+
+    /**
+     *
+     * @var string
+     */
+    public $mustChangePassword;
+
+    /**
+     *
+     * @var integer
+     */
+    public $profilesId;
+
+    /**
+     *
+     * @var string
+     */
+    public $banned;
+
+    /**
+     *
+     * @var string
+     */
+    public $suspended;
+
+    /**
+     *
+     * @var string
+     */
+    public $active;
+
+    /**
+     * Validations and business logic
+     *
+     * @return boolean
+     */
+    public function validation()
+    {
+        $validator = new Validation();
+
+        $validator->add(
+            'email',
+            new EmailValidator(
+                [
+                    'model'   => $this,
+                    'message' => 'Please enter a correct email address',
+                ]
+            )
+        );
+
+        return $this->validate($validator);
+    }
+
+    /**
+     * Initialize method for model.
+     */
+    public function initialize()
+    {
+        $this->setSchema("login");
+        $this->setSource("users");
+    }
+
+    /**
+     * Allows to query a set of records that match the specified conditions
+     *
+     * @param mixed $parameters
+     * @return Users[]|Users|\Phalcon\Mvc\Model\ResultSetInterface
+     */
+    public static function find($parameters = null): \Phalcon\Mvc\Model\ResultsetInterface
+    {
+        return parent::find($parameters);
+    }
+
+    /**
+     * Allows to query the first record that match the specified conditions
+     *
+     * @param mixed $parameters
+     * @return Users|\Phalcon\Mvc\Model\ResultInterface|\Phalcon\Mvc\ModelInterface|null
+     */
+    public static function findFirst($parameters = null): ?\Phalcon\Mvc\ModelInterface
+    {
+        return parent::findFirst($parameters);
+    }
+
+}
